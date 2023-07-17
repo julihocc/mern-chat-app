41a3ade8fdbceda75878899fd19da08b7f3a4de1
Juliho Castillo <julihocc@users.noreply.github.com>
2023-07-17 12:31:15 -0600

    "Improved ChatRoomViewer UI by styling messages, adding a scrollable view for messages, and enhancing the chat input field for better usability."

--- frontend/src/components/ChatRoomViewer.js
+++ frontend/src/components/ChatRoomViewer.js
@@ -1,10 +1,11 @@
 // ChatRoomViewer component
 // Path: frontend\src\components\ChatRoomViewer.js
+import { Box, Stack, Paper, TextField, Button, Typography, CssBaseline, Container } from '@mui/material';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { gql } from '@apollo/client';
 import { useQuery } from '@apollo/react-hooks';
-import { Typography, TextField, Button } from '@mui/material'; // Import MUI components
+// import { Typography, TextField, Button } from '@mui/material'; // Import MUI components
 import Loading from './ChatRoomViewer/Loading';
 import Error from './ChatRoomViewer/Error';
 import useChatRoomQuery from './ChatRoomViewer/useChatRoomQuery';
@@ -19,7 +20,7 @@ const GET_USER_BY_ID = gql`
     }
 `;
 
-const Message = ({ message }) => {
+const Message = ({ message, isCurrentUser }) => {
     const { loading, error, data } = useQuery(GET_USER_BY_ID, {
         variables: { userId: message.senderId },
     });
@@ -28,9 +29,18 @@ const Message = ({ message }) => {
     if (error) return <p>GET_USER)BY_ID Error: {error.message}</p>;
 
     return (
-        <li key={message.id}>
-            {data.getUserById.email}: {message.body}
-        </li>
+        <Box
+            sx={{
+                mb: 1,
+                p: 1,
+                backgroundColor: isCurrentUser ? 'primary.light' : 'grey.300',
+                color: isCurrentUser ? 'primary.contrastText' : 'grey.900',
+                alignSelf: isCurrentUser ? 'flex-end' : 'flex-start',
+            }}
+        >
+            <Typography variant="subtitle2">{data.getUserById.email}</Typography>
+            <Typography variant="body2">{message.body}</Typography>
+        </Box>
     );
 };
 
@@ -46,35 +56,58 @@ const ChatRoomViewer = () => {
         return <Loading queryName="Loading" />;
     }
 
-    // if (error) {
-    //     return <Error queryName="ChatRoomViewer Error" message={error.message} />;
-    // }
-
     if (chatRoom.error)  return <Error queryName="chatRoom" message={chatRoom.error.message} />;
     if (messages.error)  return <Error queryName="messages" message={messages.error.message} />;
     if (currentUser.error)  return <Error queryName="currentUser" message={currentUser.error.message} />;
     if (sendMessageError)  return <Error queryName="sendMessage" message={sendMessageError.message} />;
 
+    // return (
+    //     <div>
+    //         <Typography variant="h2">Chat Room: {chatRoom.data.getChatRoom.id}</Typography>
+    //         <ul>
+    //             {messages.data.getMessagesByChatRoomId.map((message) => (
+    //                 <Message key={message.id} message={message} />
+    //             ))}
+    //         </ul>
+    //         <form onSubmit={(e) => handleSendMessage(e, currentUser.data.getCurrentUser.id, chatRoom.data.getChatRoom.id)}>
+    //             <TextField
+    //                 type="body"
+    //                 label="New Message"
+    //                 value={messageBody}
+    //                 onChange={(e) => setMessageBody(e.target.value)}
+    //             />
+    //             <Button type="submit" variant="contained" color="primary">
+    //                 Send Message
+    //             </Button>
+    //         </form>
+    //     </div>
+    // );
     return (
-        <div>
-            <Typography variant="h2">Chat Room: {chatRoom.data.getChatRoom.id}</Typography>
-            <ul>
-                {messages.data.getMessagesByChatRoomId.map((message) => (
-                    <Message key={message.id} message={message} />
-                ))}
-            </ul>
+        <Container component={Paper} sx={{ height: '90vh', mt: 2, display: 'flex', flexDirection: 'column', p: 2 }}>
+            <CssBaseline />
+            <Typography variant="h2" sx={{ mb: 2 }}>Chat Room: {chatRoom.data.getChatRoom.id}</Typography>
+            <Box sx={{ overflow: 'auto', flexGrow: 1 }}>
+                <Stack>
+                    {messages.data.getMessagesByChatRoomId.map((message) => (
+                        <Message key={message.id} message={message} isCurrentUser={message.senderId === currentUser.data.getCurrentUser.id} />
+                    ))}
+                </Stack>
+            </Box>
             <form onSubmit={(e) => handleSendMessage(e, currentUser.data.getCurrentUser.id, chatRoom.data.getChatRoom.id)}>
-                <TextField
-                    type="body"
-                    label="New Message"
-                    value={messageBody}
-                    onChange={(e) => setMessageBody(e.target.value)}
-                />
-                <Button type="submit" variant="contained" color="primary">
-                    Send Message
-                </Button>
+                <Stack direction="row" spacing={1}>
+                    <TextField
+                        type="body"
+                        label="New Message"
+                        fullWidth
+                        value={messageBody}
+                        onChange={(e) => setMessageBody(e.target.value)}
+                    />
+                    <Button type="submit" variant="contained" color="primary">
+                        Send Message
+                    </Button>
+                </Stack>
             </form>
-        </div>
+        </Container>
     );
 };
 
