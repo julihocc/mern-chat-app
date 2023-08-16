4c3acf1cb23c3d96cc2df794113c2f9be1e88117
Juliho Castillo <julihocc@users.noreply.github.com>
2023-08-15 18:09:25 -0600

    Dashboard refactored making use of Redux

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -1,18 +1,24 @@
 // frontend/src/reducers.js
 // Importing the SET_USER action type and the logger utility
 import logger from './utils/logger';
-import {LOGIN_USER, LOGOUT_USER, SET_USER} from './actions';
+import {FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_FAILURE,LOGIN_USER, LOGOUT_USER, SET_USER} from './actions';
 
 // Defining the initial state of the user portion of the Redux store
 // Initially, the user is set to null
-const initialState = {
+const initialUserState = {
     user: null,
-    isLoggedIn: false // Add isLoggedIn to the initial state
+    isLoggedIn: false, // Add isLoggedIn to the initial state
 };
+
+const initialCurrentUserState = {
+    loading: false,
+    user: null,
+    error: null
+}
 
 // Defining the userReducer function to handle user-related actions
 // It takes the current state and an action as parameters
-const userReducer = (state = initialState, action) => {
+const userReducer = (state = initialUserState, action) => {
     // Logging the received action type and payload for debugging
     logger.debug('Action received in userReducer:', action.type);
     logger.debug('Action payload:', action.payload);
@@ -36,11 +42,34 @@ const userReducer = (state = initialState, action) => {
         default:
             // Logging that the action type was not handled by this reducer
             logger.debug('Unhandled action type in userReducer:', action.type);
-
             // Returning the current state unchanged
             return state;
     }
 };
 
+const currentUserReducer = (state = initialCurrentUserState, action) => {
+    // Logging the received action type and payload for debugging
+    logger.debug('Action received in userReducer:', action.type);
+    logger.debug('Action payload:', action.payload);
+
+    // Using a switch statement to handle different action types
+    switch (action.type) {
+        case FETCH_USER_REQUEST:
+            return {...state, loading: true};
+        case FETCH_USER_SUCCESS:
+            return {...state, loading: false, user: action.payload, error: null};
+        case FETCH_USER_FAILURE:
+            return {...state, loading: false, user: null, error: action.payload};
+        // Handling any other action types
+        default:
+            // Logging that the action type was not handled by this reducer
+            logger.debug('Unhandled action type in userReducer:', action.type);
+            // Returning the current state unchanged
+            return state;
+    }
+};
 // Exporting the userReducer function to be used elsewhere in the application
-export default userReducer;
+export {
+    userReducer,
+    currentUserReducer
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,21 +2,25 @@
 
 // Importing the Redux Toolkit's configureStore function
 import { configureStore } from '@reduxjs/toolkit';
-
+import thunk from 'redux-thunk'; // Import Redux Thunk
 // Importing the userReducer from the reducers file
-import userReducer from './reducers';
+import {
+    userReducer,
+    currentUserReducer
+} from './reducers';
 
 // Defining the rootReducer object, which combines all the reducers in the application
 // In this case, only the userReducer is included, but other reducers can be added as well
 const rootReducer = {
-    user: userReducer
-    // other reducers can go here
+    user: userReducer,
+    currentUser: currentUserReducer
 };
 
 // Calling the configureStore function to create the Redux store
 // Passing the rootReducer object to the reducer option to configure the store's reducers
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: [thunk]
 });
 
 // Exporting the configured store, so it can be used in other parts of the application
