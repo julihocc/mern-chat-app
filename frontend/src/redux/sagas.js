309dc0c5e1bd542c0b19ba6273c0fa491a7eb5db
Juliho Castillo <julihocc@users.noreply.github.com>
2023-09-29 06:26:23 -0600

    chat functionality without sagas

--- frontend/src/redux/sagas.js
+++ frontend/src/redux/sagas.js
@@ -8,12 +8,6 @@ import {
     fetchUserFailure,
     fetchUserSaga,
 } from './slices/userSlice';
-import {
-    fetchMessagesRequest,
-    fetchMessagesSuccess,
-    fetchMessagesFailure,
-} from './slices/chatSlice';
-import { GET_MESSAGES_BY_CHATROOM_ID } from "../gql/queries/GET_MESSAGES_BY_CHATROOM_ID";
 
 // Worker saga to fetch current user
 function* fetchCurrentUserSaga() {
@@ -26,24 +20,6 @@ function* fetchCurrentUserSaga() {
     }
 }
 
-// Worker saga for chat messages
-function* fetchChatMessagesSaga(action) {
-    yield put(fetchMessagesRequest());
-    try {
-        const { data } = yield call(apolloClient.query, {
-            query: GET_MESSAGES_BY_CHATROOM_ID,
-            variables: { chatRoomId: action.payload.chatRoomId }
-        });
-        yield put(fetchMessagesSuccess(data.getMessagesByChatRoomId));
-    } catch (error) {
-        yield put(fetchMessagesFailure(error.message));
-    }
-}
-
-// Watcher saga for chat messages
-function* watchFetchChatMessages() {
-    yield takeLatest('FETCH_MESSAGES_REQUEST', fetchChatMessagesSaga);
-}
 
 // Watcher saga to capture dispatched particular actions
 function* watchFetchUserWithSagaAction() {
@@ -53,7 +29,6 @@ function* watchFetchUserWithSagaAction() {
 // Root saga
 export default function* rootSaga() {
     yield all([
-        watchFetchChatMessages(),  // Existing watcher saga for chat messages
         watchFetchUserWithSagaAction(),  // New watcher saga for fetching user
     ]);
 }
