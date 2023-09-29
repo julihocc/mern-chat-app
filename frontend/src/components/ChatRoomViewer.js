bb777c0d173b8f53c0d3f4e6fbe289fb7f5d2798
Juliho Castillo <julihocc@users.noreply.github.com>
2023-09-29 06:54:19 -0600

    Refactoring get messages for a chat room query

--- frontend/src/components/ChatRoomViewer.js
+++ frontend/src/components/ChatRoomViewer.js
@@ -1,51 +1,51 @@
 // path: frontend/src/components/ChatRoomViewer.js
 // Importing dependencies and components
 import {
-    Stack, Paper, TextField, Button, Typography,
-    CssBaseline, Container, List, ListItem,
-    ListItemAvatar, ListItemText, Avatar,
+    Alert,
+    Avatar,
+    Button,
+    CircularProgress,
+    Container,
+    CssBaseline,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Paper,
+    Stack,
+    TextField,
+    Typography,
 } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import PersonIcon from "@mui/icons-material/Person";
 import Loading from "./Loading";
 import useGetMessagesByChatRoomId from "../hooks/queries/useGetMessagesByChatRoomId";
 import useNewMessageSubscription from "../hooks/subscriptions/useNewMessageSubscription";
-import { useTranslation } from "react-i18next";
-import { useGetChatRoomById } from "../hooks/queries/useGetChatRoomById";
-import { useGetCurrentUser } from "../hooks/queries/useGetCurrentUser";
+import {useTranslation} from "react-i18next";
+import {useGetChatRoomById} from "../hooks/queries/useGetChatRoomById";
 import logger from "loglevel";
-import { useMutation } from "@apollo/react-hooks";
-import { SEND_MESSAGE } from "../gql/mutations/SEND_MESSAGE";
+import {useMutation} from "@apollo/react-hooks";
+import {SEND_MESSAGE} from "../gql/mutations/SEND_MESSAGE";
 import {GET_MESSAGES_BY_CHAT_ROOM_ID} from "../gql/queries/GET_MESSAGES_BY_CHAT_ROOM_ID";
-import { useDispatch, useSelector } from 'react-redux';
-import { CircularProgress, Grid, Alert } from '@mui/material';
-import ChatRoomList from './ChatRoomList';
-import SendContactRequestForm from './SendContactRequestForm';
-import PendingContactRequestsList from './PendingContactRequestsList';
-import CreateGroupConversation from './CreateGroupConversation';
-import { ContactListWithFullDetails } from './ContactListWithFullDetails';
+import {useDispatch, useSelector} from 'react-redux';
 // Import the new action creator
-import { initiateFetchCurrentUser } from '../redux/actions';
+import {initiateFetchCurrentUser} from '../redux/actions';
 import log from "../utils/logger";
 
 const ChatRoomViewer = () => {
-    const { t } = useTranslation();
-    const { chatRoomId } = useParams();
+    const {t} = useTranslation();
+    const {chatRoomId} = useParams();
     const chatRoom = useGetChatRoomById(chatRoomId);
-    const { messageData, messageLoading, messageError } = useGetMessagesByChatRoomId(chatRoomId);
-    const { newMessageData } = useNewMessageSubscription();
+    const {messageData, messageLoading, messageError} = useGetMessagesByChatRoomId(chatRoomId);
+    const {newMessageData} = useNewMessageSubscription();
     const [messageBody, setMessageBody] = useState("");
     const [file, setFile] = useState(null);
     const [messages, setMessages] = useState([]);
-    const [sendMessageMutation, { loading: sendMessageLoading, error: sendMessageError }] = useMutation(SEND_MESSAGE, {
-        refetchQueries: [
-            { query: GET_MESSAGES_BY_CHAT_ROOM_ID, variables: { chatRoomId } }
-        ],
-        onCompleted: (data) => {
+    const [sendMessageMutation, {loading: sendMessageLoading, error: sendMessageError}] = useMutation(SEND_MESSAGE, {
+        refetchQueries: [{query: GET_MESSAGES_BY_CHAT_ROOM_ID, variables: {chatRoomId}}], onCompleted: (data) => {
             logger.info("Message sent successfully:", data);
-        },
-        onError: (error) => {
+        }, onError: (error) => {
             logger.error("Error sending message:", error);
         }
     });
@@ -58,29 +58,23 @@ const ChatRoomViewer = () => {
         setFile(tempFile);
     };
 
-    // Log user and chat room data
-    // logger.debug("Current user data:", currentUser);
-    // logger.debug("Chat room data:", chatRoom);
 
     const handleSendMessage = async (e, senderId, chatRoomId) => {
         e.preventDefault();
         await sendMessageMutation({
             variables: {
-                senderId: senderId,
-                chatRoomId: chatRoomId,
-                body: messageBody,
-                file: file, // Including the file in the mutation
+                senderId: senderId, chatRoomId: chatRoomId, body: messageBody, file: file,
             }
         })
     };
     useEffect(() => {
-        logger.info("Messages from server:", messageData?.getMessagesByChatRoomId); // Log messages
+        logger.info("Messages from server:", messageData?.getMessagesByChatRoomId);
         setMessages(messageData?.getMessagesByChatRoomId);
     }, [messageData]);
 
     useEffect(() => {
         if (newMessageData?.newMessage) {
-            logger.info("New message from subscription:", newMessageData.newMessage); // Log new message
+            logger.info("New message from subscription:", newMessageData.newMessage);
             setMessages((prev) => [...prev, newMessageData.newMessage]);
         }
     }, [newMessageData]);
@@ -90,7 +84,7 @@ const ChatRoomViewer = () => {
     const dispatch = useDispatch();
 
     // Extracting relevant pieces of the state
-    const { loading, user, error, isLoggedIn } = useSelector((state) => state.user);
+    const {loading, user, error, isLoggedIn} = useSelector((state) => state.user);
 
     // Fetch current user details if logged in
     useEffect(() => {
@@ -101,7 +95,7 @@ const ChatRoomViewer = () => {
     }, [dispatch, isLoggedIn]);
 
     // Show a loader while the request is in progress
-    if (loading) return <CircularProgress />;
+    if (loading) return <CircularProgress/>;
 
     // Handle error by showing an alert and logging it
     if (error) {
@@ -128,43 +122,43 @@ const ChatRoomViewer = () => {
     // =======================================
 
     if (!currentUserId) return <Typography variant="h4">Please log in to view the chat room</Typography>;
-    if (isLoading) return <Loading queryName="Loading" />;
+    if (isLoading) return <Loading queryName="Loading"/>;
 
-    if(chatRoom.error) return <p>Chat Room Error: {JSON.stringify(chatRoom.error)}</p>;
-    if(messageError) return <p>Message Error: {JSON.stringify(messageError)}</p>;
-    if(sendMessageError) return <p>Send Message Error: {JSON.stringify(sendMessageError)}</p>;
+    if (chatRoom.error) return <p>Chat Room Error: {JSON.stringify(chatRoom.error)}</p>;
+    if (messageError) return <p>Message Error: {JSON.stringify(messageError)}</p>;
+    if (sendMessageError) return <p>Send Message Error: {JSON.stringify(sendMessageError)}</p>;
 
-    return (
-        <Container component={Paper} sx={{ height: "90vh", mt: 2, display: "flex", flexDirection: "column", p: 2 }}>
-            <CssBaseline />
-            <Typography variant="h2" sx={{ mb: 2 }}>{t("chatRoom")}: {chatRoom.data.getChatRoomById.id}</Typography>
-            <Typography variant="h5" sx={{ mb: 2 }}>{t("messages")}</Typography>
+    return (<Container component={Paper} sx={{height: "90vh", mt: 2, display: "flex", flexDirection: "column", p: 2}}>
+            <CssBaseline/>
+            <Typography variant="h2" sx={{mb: 2}}>{t("messages")}</Typography>
+        <Typography variant="h3" sx={{mb: 2}}>{t("chatRoom")}: {chatRoom.data.getChatRoomById.id}</Typography>
             <List>
-                {messages && messages.slice(-5).map((message, index) => (
-                    <ListItem key={index} sx={{ flexDirection: message.senderId === currentUserId ? "row-reverse" : "row" }}>
+                {messages && messages.slice(-5).map((message, index) => (<ListItem key={index}
+                                                                                   sx={{flexDirection: message.senderId === currentUserId ? "row-reverse" : "row"}}>
                         <ListItemAvatar>
-                            <Avatar><PersonIcon /></Avatar>
+                            <Avatar><PersonIcon/></Avatar>
                         </ListItemAvatar>
-                        <ListItemText primary={message.body} secondary={message.senderId} sx={{ textAlign: message.senderId === currentUserId ? "right" : "left" }} />
-                        {message.fileContent && (
-                            // Assuming the file is an image, render it as an image tag
-                            <img src={`data:${message.mimeType};base64,${message.fileContent}`} alt="Uploaded content" />
+                        <ListItemText primary={message.body} secondary={message.senderId}
+                                      sx={{textAlign: message.senderId === currentUserId ? "right" : "left"}}/>
+                        {message.fileContent && (// Assuming the file is an image, render it as an image tag
+                            <img src={`data:${message.mimeType};base64,${message.fileContent}`} alt="Uploaded content"/>
                             // For other file types, you can create a download link with the appropriate MIME type
                         )}
-                    </ListItem>
-                ))}
+                    </ListItem>))}
             </List>
 
             <form onSubmit={(e) => handleSendMessage(e, currentUserId, chatRoomId)}>
                 <Stack direction="row" spacing={1}>
-                    <TextField type="body" label={t("newMessage")} fullWidth value={messageBody} onChange={(e) => setMessageBody(e.target.value)} />
-                    <input accept="image/*" style={{ display: 'none' }} id="inputForFile" type="file" onChange={handleFileChange} />
-                    <label htmlFor="inputForFile"><Button variant="contained" component="span">{file ? file.name : 'Upload File'}</Button></label>
+                    <TextField type="body" label={t("newMessage")} fullWidth value={messageBody}
+                               onChange={(e) => setMessageBody(e.target.value)}/>
+                    <input accept="image/*" style={{display: 'none'}} id="inputForFile" type="file"
+                           onChange={handleFileChange}/>
+                    <label htmlFor="inputForFile"><Button variant="contained"
+                                                          component="span">{file ? file.name : 'Upload File'}</Button></label>
                     <Button type="submit" variant="contained" color="primary">{t("sendMessage")}</Button>
                 </Stack>
             </form>
-        </Container>
-    );
+        </Container>);
 };
 
 export default ChatRoomViewer;
