6d953ba92523d1538dc6475755cc8d3e45a6e55e
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-02 10:26:38 -0600

    change username mutation

---
+++ backend/src/graphql/mutations/changeUsername.js
@@ -0,0 +1,33 @@
+const User = require('../../models/UserModel');
+const {AuthenticationError} = require('apollo-server-express');
+const {getUserFromToken} = require("../utils")
+const logger = require("../../logger");
+
+const changeUsername = async (_, args, context) => {
+    const {newUsername} = args;
+    logger.debug(`New Username: ${newUsername}`);
+    const {token} = context;
+    if (!token) {
+        throw new AuthenticationError('You are not logged in');
+    }
+    const user = await getUserFromToken(token);
+    if (!user) {
+        throw new AuthenticationError('Not user found');
+    }
+
+    const existingUser = await User.findOne({username: newUsername});
+    if (existingUser) {
+        throw new Error('Username already taken');
+    }
+
+    try {
+        logger.debug(`Updating username: ${user.username} to ${newUsername}`);
+        await User.updateOne({username: user.username}, {$set: {username: newUsername}});
+        logger.debug(`Username updated to ${user.username}`);
+        return user;
+    } catch (err) {
+        throw new Error(`Error updating username: ${err.message}`);
+    }
+}
+
+module.exports = {changeUsername};
