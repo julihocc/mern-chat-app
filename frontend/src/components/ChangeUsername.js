50ad36980d12aea54a28b4d772ef5b8392e30ab2
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-02 13:19:09 -0600

    web browser needs to be refreshed to display username changes

--- frontend/src/components/ChangeUsername.js
+++ frontend/src/components/ChangeUsername.js
@@ -1,18 +1,34 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useChangeUsername} from "../hooks/mutations/useChangeUsername";
-import { Button, TextField } from '@mui/material';
+import {Alert, Button, TextField} from '@mui/material';
+import {useTranslation} from "react-i18next";
+import logger from "../utils/logger";
 export const ChangeUsername = () => {
+    const { t } = useTranslation();
     const [newUsername, setNewUsername] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [usernameHasChanged, setUsernameHasChanged] = useState(false);
     const changeUsername = useChangeUsername(newUsername);
 
     const handleSubmit = async () => {
+        setIsSubmitting(true);
         try {
             await changeUsername({ variables: { newUsername } });
-            // Handle success, e.g., show a success message or redirect
+            await setUsernameHasChanged(true);
+            // window.location.reload();
         } catch (error) {
-            // Handle error, e.g., show an error message
+            throw new Error(`Could not change username`)
+        } finally {
+            await setIsSubmitting(false);
         }
     };
+
+    useEffect(() => {
+        if (usernameHasChanged) {
+            // window.location.reload();
+            logger.info(`Username has been changed to ${newUsername}`);
+        }
+    }, [usernameHasChanged]);
 
     return (
         <div>
