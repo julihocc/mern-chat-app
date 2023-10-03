110a47e7403469ca5751231c9f065785df142b28
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-03 12:32:38 -0600

    gravatar used on dashboard

---
+++ frontend/src/components/Gravatar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import md5 from 'md5';
+
+export const Gravatar = ({ email, size = 80 }) => {
+    const hash = md5(email.trim().toLowerCase());
+    const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=${size}`;
+
+    return <img src={gravatarUrl} alt="User Avatar" />;
+};
