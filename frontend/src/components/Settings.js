faed5817e3f4f6a745420bec66cf2d60277030a8
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-04 13:47:01 -0600

    ChangePassword component completed

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Grid} from "@mui/material";
 import {ChangeUsername} from "./ChangeUsername";
+import {ChangePassword} from "./ChangePassword";
 
 const Settings = () => {
     return (
         <div>
-        <Grid>
-            <Grid item>
-                {/*TODO Fixed the needing of refreshing to see new username*/}
-                <ChangeUsername/>
+            <Grid>
+                <Grid item>
+                    {/*TODO Fixed the needing of refreshing to see new username*/}
+                    <ChangeUsername/>
+                </Grid>
+                <Grid item>
+                    <ChangePassword/>
+                </Grid>
             </Grid>
-        </Grid>
         </div>
     )
 }
