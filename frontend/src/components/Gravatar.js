a983a41520d3d446122b0286132c3457d6e973b4
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-09 17:52:57 -0600

    mongodb problem partially solved with mongodb atlas

--- frontend/src/components/Gravatar.js
+++ frontend/src/components/Gravatar.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import md5 from 'md5';
 import {useSelector} from "react-redux";
+import logger from "../utils/logger";
 
+// export const Gravatar = () => {
+//     const {email} = useSelector((state) => state.user);
+//     const hash = md5(email);
+//     const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=${80}`;
+//
+//     return <img src={gravatarUrl} alt="User Avatar" />;
+// };
 export const Gravatar = () => {
     const {email} = useSelector((state) => state.user);
-    const hash = md5(email);
-    const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=${80}`;
-
-    return <img src={gravatarUrl} alt="User Avatar" />;
+    if (!email) {
+        // return <div>No Email</div>;
+        logger.debug(`No Email`);
+    }
+    try {
+        const hash = md5(email);
+        const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=${80}`;
+        return <img src={gravatarUrl} alt="User Avatar"/>;
+    } catch {
+        return <div>Error generating Gravatar</div>;
+        logger.debug(`Error generating Gravatar`);
+    }
 };
