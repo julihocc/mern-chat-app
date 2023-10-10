919e94a677a17f9e3f329d5922229198e2b4ec35
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-10 10:40:12 -0600

    Update MainRoutes.js

--- frontend/src/MainRoutes.js
+++ frontend/src/MainRoutes.js
@@ -3,7 +3,18 @@
 import {useDispatch, useSelector} from 'react-redux'; // Import Redux hooks
 import {Link, Navigate, Route, Routes, useNavigate} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
-import {AppBar, Button, Container, Toolbar, Typography} from '@mui/material';
+import {
+    AppBar,
+    Button,
+    Container,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Toolbar,
+    Typography
+} from '@mui/material';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ChatRoomViewer from "./components/ChatRoomViewer";
@@ -11,6 +22,8 @@ import Signup from "./components/SignUp";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import {logoutUser, setUser} from './redux/slices/userSlice';
 import Settings from "./components/Settings";
+import {useState} from "react";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const MainRoutes = () => {
     const dispatch = useDispatch();
@@ -28,66 +41,66 @@ const MainRoutes = () => {
         dispatch(setUser(userData)); // Dispatch login action with user data
     };
 
-    return (
-        <>
-            <Container> <AppBar position="static">
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setDrawerOpen(!drawerOpen);
+    }
+
+    return (<>
+        <Container>
+            <AppBar position="static">
                 <Toolbar>
+                    <IconButton edge="start" color="inherit" onClick={toggleDrawer}>
+                        <MenuIcon/>
+                    </IconButton>
                     <Typography variant="h6" style={{flexGrow: 1}}>
                         {t("welcome")}
                     </Typography>
                     <LanguageSwitcher/>
-                    {isLoggedIn ? (
-                        <>
-                            <Button color="inherit" onClick={handleLogout}>
-                                {t("logout")}
-                            </Button>
-                            <Button color="inherit" component={Link} to="/dashboard">
-                                {t("dashboard")}
-                            </Button>
-                        </>
-                    ) : (
-                        <>
-                            <Button color="inherit" component={Link} to="/login">
-                                {t("login")}
-                            </Button>
-                            <Button color="inherit" component={Link} to="/signup">
-                                {t("signup")}
-                            </Button>
-                        </>
-                    )}
+                    {isLoggedIn ? (<>
+                        <Button color="inherit" onClick={handleLogout}>
+                            {t("logout")}
+                        </Button>
+                        <Button color="inherit" component={Link} to="/dashboard">
+                            {t("dashboard")}
+                        </Button>
+                    </>) : (<>
+                        <Button color="inherit" component={Link} to="/login">
+                            {t("login")}
+                        </Button>
+                        <Button color="inherit" component={Link} to="/signup">
+                            {t("signup")}
+                        </Button>
+                    </>)}
                 </Toolbar>
-            </AppBar> </Container>
+            </AppBar>
+            <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+                <List>
+                    <ListItem button component={Link} to="/dashboard">
+                        <ListItemText primary="Dashboard" />
+                    </ListItem>
+                </List>
+            </Drawer>
+        </Container>
 
-            <Container style={{paddingTop: '64px'}}>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            isLoggedIn ? (
-                                <Dashboard onLogout={handleLogout}/>
-                            ) : (
-                                <Navigate to="/" replace/>
-                            )
-                        }
-                    />
-                    <Route path="/signup" element={<Signup onLogin={handleLogin}/>}/>
-                    <Route path="/login" element={<Login onLogin={handleLogin}/>}/>
-                    <Route
-                        path="/dashboard"
-                        element={
-                            isLoggedIn ? (
-                                <Dashboard onLogout={handleLogout}/>
-                            ) : (
-                                <Navigate to="/" replace/>
-                            )
-                        }
-                    />
-                    <Route path="/chat/:chatRoomId" element={<ChatRoomViewer/>}/>
-                    <Route path="/settings" element={<Settings/>}/>
-                </Routes>
-            </Container>
-        </>
-    );
+        <Container style={{paddingTop: '64px'}}>
+            <Routes>
+                <Route
+                    path="/"
+                    element={isLoggedIn ? (<Dashboard onLogout={handleLogout}/>) : (<Navigate to="/" replace/>)}
+                />
+                <Route path="/signup" element={<Signup onLogin={handleLogin}/>}/>
+                <Route path="/login" element={<Login onLogin={handleLogin}/>}/>
+                <Route
+                    path="/dashboard"
+                    element={isLoggedIn ? (<Dashboard onLogout={handleLogout}/>) : (<Navigate to="/" replace/>)}
+                />
+                <Route path="/chat/:chatRoomId" element={<ChatRoomViewer/>}/>
+                <Route path="/settings" element={<Settings/>}/>
+            </Routes>
+        </Container>
+    </>);
 };
 
 export default MainRoutes;
