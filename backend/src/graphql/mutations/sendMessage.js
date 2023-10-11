cca5ecd24a5a73b4cecea117aff8d8b6eedf9c47
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-11 13:32:03 -0600

    sendMessage has been updated to use token

--- backend/src/graphql/mutations/sendMessage.js
+++ backend/src/graphql/mutations/sendMessage.js
@@ -1,69 +1,59 @@
 // Path: backend/src/graphql/mutations/sendMessage.js
-const mongoose = require("mongoose"); // Import mongoose
 const ChatRoom = require("../../models/ChatRoomModel");
 const Message = require("../../models/MessageModel");
 const logger = require("../../logger");
-const User = require("../../models/UserModel");
-const { AuthenticationError } = require("apollo-server-express");
+const {AuthenticationError} = require("apollo-server-express");
+const {getUserFromToken} = require("../utils");
 
-const sendMessage = async (
-  _,
-  { senderId, chatRoomId, body, file },
-  context,
-) => {
-  const { token, pubSub } = context;
-  if (!token) {
-    throw new AuthenticationError("You must be logged in!");
-  }
-
-  // Log the inputs without fileUrl, as it's not defined yet
-  logger.debug(
-    `Received sendMessage request with senderId: ${senderId}, chatRoomId: ${chatRoomId}, body: ${body}`,
-  );
-
-  // Handle file upload if provided
-  let fileContent = null;
-  if (file) {
-    const { createReadStream } = await file;
-    const stream = createReadStream();
-    const chunks = [];
-    for await (const chunk of stream) {
-      chunks.push(chunk);
+const sendMessage = async (_, {chatRoomId, body, file}, context) => {
+    const {token, pubSub} = context;
+    if (!token) {
+        throw new AuthenticationError("You must be logged in!");
     }
-    fileContent = Buffer.concat(chunks).toString("base64");
-    // Convert the file content to Base64
-  }
 
-  // Existing logic for sending a message
-  const chatRoom = await ChatRoom.findById(chatRoomId);
-  if (!chatRoom) {
-    logger.error("Chat room not found"); // Log this error
-    throw new Error("Chat room not found");
-  }
+    const sender = getUserFromToken(token);
 
-  const sender = await User.findById(senderId);
+    const senderId = sender.id;
 
-  const message = new Message({
-    chatRoomId: chatRoom.id,
-    senderId: sender.id,
-    body: body,
-    fileContent: fileContent, // Include file URL
-  });
+    // Log the inputs without fileUrl, as it's not defined yet
+    logger.debug(`Received sendMessage request with senderId: ${senderId}, chatRoomId: ${chatRoomId}, body: ${body}`,);
 
-  try {
-    await message.save();
-    logger.debug(`Message saved with id: ${message.id}`); // Log this info
-  } catch (err) {
-    logger.error(`Failed to save message: ${err}`); // Log this error
-    throw new Error("Failed to save message"); // Propagate the error to the client
-  }
+    // Handle file upload if provided
+    let fileContent = null;
+    if (file) {
+        const {createReadStream} = await file;
+        const stream = createReadStream();
+        const chunks = [];
+        for await (const chunk of stream) {
+            chunks.push(chunk);
+        }
+        fileContent = Buffer.concat(chunks).toString("base64");
+    }
 
-  chatRoom.messageIds.push(message.id);
-  await chatRoom.save();
+    const chatRoom = await ChatRoom.findById(chatRoomId);
+    if (!chatRoom) {
+        logger.error("Chat room not found");
+        throw new Error("Chat room not found");
+    }
 
-  pubSub.publish(`NEW_MESSAGE_${chatRoomId}`, { newMessage: message });
+    const message = new Message({
+        chatRoomId: chatRoom.id, senderId: sender.id, body: body, fileContent: fileContent, // Include file URL
+    });
 
-  return message;
+    try {
+        await message.save();
+        logger.debug(`Message saved with id: ${message.id}`); // Log this info
+    } catch (err) {
+        logger.error(`Failed to save message: ${err}`); // Log this error
+        throw new Error("Failed to save message"); // Propagate the error to the client
+    }
+
+    chatRoom.messageIds.push(message.id);
+    await chatRoom.save();
+
+    pubSub.publish(`NEW_MESSAGE_${chatRoomId}`, {newMessage: message});
+
+    return message;
 };
 
-module.exports = { sendMessage };
+module.exports = {sendMessage};
--- backend/src/graphql/utils.js
+++ backend/src/graphql/utils.js
@@ -3,17 +3,15 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/UserModel");
-const logger = require("../logger"); // Import the logger
+const logger = require("../logger");
 const saltRounds = 10;
 
 const encryptPassword = async (password) => {
-  // return await bcrypt.hash(password, saltRounds);
-  return password;
+  return await bcrypt.hash(password, saltRounds);
 };
 
 const comparePassword = async (password, hash) => {
-  // return await bcrypt.compare(password, hash);
-  return password === hash;
+  return await bcrypt.compare(password, hash);
 };
 
 const getUserFromToken = async (token) => {
@@ -22,7 +20,7 @@ const getUserFromToken = async (token) => {
     const decoded = jwt.verify(tokenString, process.env.JWT_SECRET);
     return User.findById(decoded.id);
   } catch (err) {
-    logger.error("Error in getUserFromToken", err); // Log the error
+    logger.error("Error in getUserFromToken", err);
     throw new Error(err);
   }
 };
