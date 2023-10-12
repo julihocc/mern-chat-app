b97de36f53b718430a07dd889cbd46e480c6c00c
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-12 10:11:42 -0600

    implementing cookie solution

--- backend/src/graphql/mutations/login.js
+++ backend/src/graphql/mutations/login.js
@@ -2,22 +2,28 @@
 const User = require("../../models/UserModel");
 const jwt = require("jsonwebtoken");
 const { comparePassword } = require("../utils");
-const logger = require("../../logger"); // add this line to import the logger
+const logger = require("../../logger");
 
-const login = async (parent, { email, password }) => {
+const login = async (_, { email, password }, context) => {
   const user = await User.findOne({ email });
   if (!user) {
-    logger.error(`Invalid email: ${email}`); // log the error
+    logger.error(`Invalid email: ${email}`);
     throw new Error("Invalid email");
   }
   const match = await comparePassword(password, user.password);
   if (!match) {
-    logger.error(`Invalid password for email: ${email}`); // log the error
+    logger.error(`Invalid password for email: ${email}`);
     throw new Error("Invalid password");
   }
   const payload = { id: user._id, email: user.email };
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
-  //logger.debug(`User logged in: ${email}`); // log successful login
+
+  context.res.cookie('authToken', token, {
+    httpOnly: true,
+    maxAge: 3600000,
+    sameSite: 'lax'
+  });
+
   return {
     token,
     user,
