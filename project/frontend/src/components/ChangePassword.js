26dd243eaabb521cfcb88df2091c483c0299c6b0
Juliho Castillo <julihocc@users.noreply.github.com>
2023-11-09 16:52:45 -0600

    password before changing name not working

--- project/frontend/src/components/ChangePassword.js
+++ project/frontend/src/components/ChangePassword.js
@@ -1,52 +1,30 @@
-import React, { useState } from "react";
-import { useChangePassword } from "../hooks/mutations/useChangePassword";
-import {Box, Button, TextField} from "@mui/material";
+import React, {useState} from "react";
+import {useChangePassword} from "../hooks/mutations/useChangePassword";
+import {Box, Button, TextField, Typography} from "@mui/material";
 
 export const ChangePassword = () => {
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-  const { changePassword } = useChangePassword();
+    const {changePassword} = useChangePassword();
 
-  const handlePasswordChange = async () => {
-    try {
-      if (newPassword !== confirmPassword) {
-        throw new Error("Passwords do not match");
-      }
-      await changePassword({
-        variables: { oldPassword: currentPassword, newPassword },
-      });
-    } catch (error) {
-      throw new Error(`Could not change password: ${error.message}`);
-    }
-  };
+    const handlePasswordChange = async () => {
+        try {
+            if (newPassword !== confirmPassword) {
+                throw new Error("Passwords do not match");
+            }
+            await changePassword({
+                variables: {oldPassword: currentPassword, newPassword},
+            });
+        } catch (error) {
+            throw new Error(`Could not change password: ${error.message}`);
+        }
+    };
 
-  // return (
-  //   <div>
-  //     <input
-  //       type="password"
-  //       placeholder="Current Password"
-  //       value={currentPassword}
-  //       onChange={(e) => setCurrentPassword(e.target.value)}
-  //     />
-  //     <input
-  //       type="password"
-  //       placeholder="New Password"
-  //       value={newPassword}
-  //       onChange={(e) => setNewPassword(e.target.value)}
-  //     />
-  //     <input
-  //       type="password"
-  //       placeholder="Confirm New Password"
-  //       value={confirmPassword}
-  //       onChange={(e) => setConfirmPassword(e.target.value)}
-  //     />
-  //     <button onClick={handlePasswordChange}>Change Password</button>
-  //   </div>
-  // );
     return (
         <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+            <Typography variant="h6">Change Password</Typography>
             <TextField
                 type="password"
                 label="Current Password"
--- project/frontend/src/gql/mutations/CHANGE_USERNAME.js
+++ project/frontend/src/gql/mutations/CHANGE_USERNAME.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag";
 
 export const CHANGE_USERNAME = gql`
-  mutation ChangeUsername($newUsername: String!) {
-    changeUsername(newUsername: $newUsername) {
+  mutation ChangeUsername($newUsername: String!, $currentPassword: String!) {
+    changeUsername(newUsername: $newUsername, currentPassword: $current) {
       username
     }
   }
