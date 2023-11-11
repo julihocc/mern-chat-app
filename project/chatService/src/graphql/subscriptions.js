2367b8631619a88f30361e64ef2784dbfb7f790c
Juliho Castillo <julihocc@users.noreply.github.com>
2023-11-10 19:27:02 -0600

    Template for chat service

---
+++ project/chatService/src/graphql/subscriptions.js
@@ -0,0 +1,14 @@
+// backend\src\graphql\resolvers\subscriptions.js
+const logger = require("../utils/logger"); // Import the logger
+
+const subscriptions = {
+  newMessage: {
+    subscribe: (parent, { chatRoomId }, { pubSub }) => {
+      //logger.debug('loading newMessage'); // Log the info
+      //logger.debug(`newMessage: ${chatRoomId}`); // Log the info
+      return pubSub.asyncIterator(`NEW_MESSAGE_${chatRoomId}`);
+    },
+  },
+};
+
+module.exports = subscriptions;
