034cb9e4293beef2a0d693714a3d35bb4eb2ada9
Juliho Castillo <julihocc@users.noreply.github.com>
2023-11-17 14:08:44 -0600

    Update index.js

--- project/gateway/src/index.js
+++ project/gateway/src/index.js
@@ -1,26 +1,37 @@
-const { ApolloServer } = require('apollo-server');
-const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
+const {ApolloServer} = require('apollo-server');
+const {ApolloGateway, IntrospectAndCompose} = require('@apollo/gateway');
 
-const gateway = new ApolloGateway({
-	supergraphSdl: new IntrospectAndCompose({
-		subgraphs: [
-			{ name: 'auth', url: 'http://localhost:4001/graphql' },
-			// Add other services here as you federate them
-		],
-	}),
-});
 
-const server = new ApolloServer({
-	gateway,
-	subscriptions: false,
-	context: ({req}) => {
-		return { headers: req.headers };
+async function startServer() {
+
+	const gateway = new ApolloGateway({
+		supergraphSdl: new IntrospectAndCompose({
+			subgraphs: [
+				{name: 'auth', url: 'http://localhost:4001/graphql'},
+				// Add other services here as you federate them
+			],
+		}),
+	});
+
+	const server = new ApolloServer({
+		gateway,
+		subscriptions: false,
+		context: ({req}) => {
+			return { headers: req.headers };
+		}
+	});
+
+	server.listen().then(({url}) => {
+		console.log(`🚀 Server ready at ${url}`);
+	});
+
+
+}
+
+(async () => {
+	try {
+		await startServer();
+	} catch (err) {
+		console.error("Error starting the authentication service:", err);
 	}
-});
-
-server.listen().then(({ url }) => {
-	console.log(`🚀 Server ready at ${url}`);
-});
-
-
-
+})();
