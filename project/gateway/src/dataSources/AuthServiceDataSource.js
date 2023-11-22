4dbbbc012a3c6454a897d9adb3904fbee93a7fbe
Juliho Castillo <julihocc@users.noreply.github.com>
2023-11-22 13:11:34 -0600

    Update AuthServiceDataSource.js

--- project/gateway/src/dataSources/AuthServiceDataSource.js
+++ project/gateway/src/dataSources/AuthServiceDataSource.js
@@ -1,32 +1,25 @@
 // AuthServiceDataSource.js
-const { RESTDataSource } = require("@apollo/datasource-rest");
+const {RESTDataSource} = require("@apollo/datasource-rest");
 
 class AuthAPI extends RESTDataSource {
 	constructor(options) {
 		super(options);
-		this.baseURL = 'http://localhost:5000'; // Replace with your AuthService URL
+		this.baseURL = 'http://localhost:5000/graphql'; // Replace with your AuthService URL
 	}
 
 	async login(email, password) {
+		console.log(email, password);
 		const response = await this.post('', {
-			query: `
-            mutation Login($email: String!, $password: String!) {
-                login(email: $email, password: $password) {
-                    user {
-                        email
-                    }
-                    token
-                }
-            }
-        `,
-			variables: {
-				email,
-				password
-			}
+			body: {
+				query: "mutation Mutation($email: String!, $password: String!) {  login(email: $email, password: $password) {    user {      email    }    token  } }",
+				variables: {
+					email, password,
+				}
+			},
 		});
+		console.log(response);
 		return response.data.login;
 	}
-
 
 }
 
