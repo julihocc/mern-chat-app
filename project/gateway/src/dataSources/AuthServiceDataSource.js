28309bef3b6d96f662cba52bdb6381bcfcda29dd
Juliho Castillo <julihocc@users.noreply.github.com>
2023-11-22 17:45:31 -0600

    Update AuthServiceDataSource.js

--- project/gateway/src/dataSources/AuthServiceDataSource.js
+++ project/gateway/src/dataSources/AuthServiceDataSource.js
@@ -4,7 +4,7 @@ const {RESTDataSource} = require("@apollo/datasource-rest");
 class AuthAPI extends RESTDataSource {
 	constructor(options) {
 		super(options);
-		this.baseURL = 'http://localhost:5000/graphql'; // Replace with your AuthService URL
+		this.baseURL = 'http://localhost:5000/graphql';
 	}
 
 	async login(email, password) {
