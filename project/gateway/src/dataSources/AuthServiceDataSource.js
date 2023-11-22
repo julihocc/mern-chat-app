b82b896d79b2f0cc646dce33ef5f964050a124e5
Juliho Castillo <julihocc@users.noreply.github.com>
2023-11-22 11:12:21 -0600

    Update AuthServiceDataSource.js

--- project/gateway/src/dataSources/AuthServiceDataSource.js
+++ project/gateway/src/dataSources/AuthServiceDataSource.js
@@ -4,78 +4,11 @@ const { RESTDataSource } = require("@apollo/datasource-rest");
 class AuthAPI extends RESTDataSource {
 	constructor(options) {
 		super(options);
-		this.baseURL = 'http://localhost:5000/'; // Replace with your AuthService URL
+		this.baseURL = 'http://localhost:5000/graphql'; // Replace with your AuthService URL
 	}
 
 	async login(email, password) {
-		return this.post('graphql', {
-			query: `
-        mutation {
-          login(email: "${email}", password: "${password}") {
-            token
-            user {
-              id
-              email
-            }
-          }
-        }
-      `
-		});
-	}
-
-	async signUp(username, email, password) {
-		return this.post('graphql', {
-			query: `
-        mutation {
-          signUp(username: "${username}", email: "${email}", password: "${password}") {
-            token
-            user {
-              id
-              email
-            }
-          }
-        }
-      `
-		});
-	}
-
-	async changePassword(userId, newPassword) {
-		return this.post('graphql', {
-			query: `
-        mutation {
-          changePassword(userId: "${userId}", newPassword: "${newPassword}") {
-            success
-            message
-          }
-        }
-      `
-		});
-	}
-
-	async changeUsername(userId, newUsername) {
-		return this.post('graphql', {
-			query: `
-        mutation {
-          changeUsername(userId: "${userId}", newUsername: "${newUsername}") {
-            success
-            message
-          }
-        }
-      `
-		});
-	}
-
-	async logout(userId) {
-		return this.post('graphql', {
-			query: `
-        mutation {
-          logout(userId: "${userId}") {
-            success
-            message
-          }
-        }
-      `
-		});
+		return this.post('login', { email, password });
 	}
 
 }
