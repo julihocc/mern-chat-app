347c0237d258465ba0ff3fa2369e476910924d06
Juliho Castillo <julihocc@users.noreply.github.com>
2023-12-04 19:09:39 -0600

    Update field name in GraphQL query for chatrooms
    The field name in the getChatRoomsForCurrentUser query was updated from 'id' to '_id'. This change was made to accurately reflect the database schema, which uses the '_id' identifier. This ensures that the GraphQL query will successfully fetch the list of user-associated chatrooms.

--- project/frontend/src/gql/queries/GET_CHAT_ROOMS_FOR_CURRENT_USER.js
+++ project/frontend/src/gql/queries/GET_CHAT_ROOMS_FOR_CURRENT_USER.js
@@ -3,7 +3,7 @@ import gql from "graphql-tag";
 export const GET_CHAT_ROOMS_FOR_CURRENT_USER = gql`
   query GetChatRoomsForCurrentUser {
     getChatRoomsForCurrentUser {
-      id
+      _id
       participantIds
       messageIds
       createdAt
