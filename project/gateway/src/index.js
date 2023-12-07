c7fea74520d0e469e1fbd927c0b9ecfe2a2ef75c
Juliho Castillo <julihocc@users.noreply.github.com>
2023-12-07 11:45:33 -0600

    Disable CORS and add Dockerfile for deployment
    In gateway/src/index.js, the CORS middleware options have been commented out to prevent cross-origin restrictions while the application is in a development environment. A Dockerfile has also been added to the gateway directory to enable application containment and ensure a consistent running environment across multiple systems. Deployment in Dockerized environment will allow for greater scalability and operational efficiency.

--- project/gateway/src/index.js
+++ project/gateway/src/index.js
@@ -22,13 +22,13 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-const corsOptions = {
-	origin: "*",
-	credentials: true,
-	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-	allowedHeaders: ["Content-Type", "Authorization"],
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+// 	origin: "*",
+// 	credentials: true,
+// 	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+// 	allowedHeaders: ["Content-Type", "Authorization"],
+// };
+// app.use(cors(corsOptions));
 
 const apiLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 1 hour window
