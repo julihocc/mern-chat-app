79e969cdfe0fb7f280f6d4e26226b5e6d7573499
Juliho Castillo <julihocc@users.noreply.github.com>
2024-01-17 14:14:53 -0600

    Apollo server has been updated

--- code/gateway/src/index.js
+++ code/gateway/src/index.js
@@ -1,14 +1,13 @@
-// project/gateway/src/index.js
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const { PubSub } = require("graphql-subscriptions");
 const { execute, subscribe } = require("graphql");
-const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { useServer } = require("graphql-ws/lib/use/ws");
+const { WebSocketServer } = require("ws");
+const http = require("http");
 const { typeDefs } = require("./graphql/typeDefs");
 const { resolvers } = require("./graphql/resolvers");
 const errorHandler = require("./utils/errorHandler");
-const http = require("http");
-const PORT = process.env.PORT || 3001;
 const cookieParser = require("cookie-parser");
 const logger = require("./utils/logger");
 const { graphqlUploadExpress } = require("graphql-upload");
@@ -16,9 +15,8 @@ const rateLimit = require("express-rate-limit");
 const { AuthAPI } = require("./dataSources/AuthServiceDataSource");
 const { ChatAPI } = require("./dataSources/ChatServiceDataSource");
 const { ContactAPI } = require("./dataSources/ContactServiceDataSource");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
-const { log } = require("console");
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
@@ -30,11 +28,8 @@ const apiLimiter = rateLimit({
 });
 
 app.use("/graphql", apiLimiter);
-
 app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
-
 app.use(errorHandler);
-
 app.use(cookieParser());
 
 app.use((req, res, next) => {
@@ -50,64 +45,44 @@ async function startServer() {
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-
-    // introspection: true,
     context: ({ req, res, connection }) => {
-      const pubSub = new PubSub();
-      logger.debug(`pubSub: ${JSON.stringify(pubSub)}`);
       if (connection) {
-        return { ...connection.context, pubSub };
+        return { ...connection.context };
       } else {
         const token = req.headers.authorization || "";
         req.token = token;
-        return { req, res, pubSub, token };
+        return { req, res, token };
       }
     },
-
     dataSources: () => ({
       authAPI: new AuthAPI(),
       chatAPI: new ChatAPI(),
       contactAPI: new ContactAPI(),
     }),
-
     uploads: false,
   });
 
   await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
-  return apolloServer;
+  const httpServer = http.createServer(app);
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: apolloServer.graphqlPath,
+  });
+
+  useServer({ schema, execute, subscribe }, wsServer);
+
+  httpServer.listen(PORT, () => {
+    logger.debug(
+        `Server is running at http://localhost:${PORT}${apolloServer.graphqlPath}`
+    );
+    logger.debug(
+        `Subscriptions ready at ws://localhost:${PORT}${apolloServer.graphqlPath}`
+    );
+  });
 }
 
-(async () => {
-  try {
-    const apolloServer = await startServer();
-    apolloServer.applyMiddleware({ app });
-    const httpServer = http.createServer(app);
-
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-    SubscriptionServer.create(
-      {
-        // schema: apolloServer.schema,
-        schema,
-        execute,
-        subscribe,
-      },
-      {
-        server: httpServer,
-        path: apolloServer.graphqlPath,
-      }
-    );
-
-    httpServer.listen(PORT, () => {
-      logger.debug(
-        `Server is running at http://localhost:${PORT}${apolloServer.graphqlPath}`
-      );
-      logger.debug(
-        `Subscriptions ready at ws://localhost:${PORT}${apolloServer.graphqlPath}`
-      );
-    });
-  } catch (err) {
-    logger.error("Error starting the authentication service:", err);
-  }
-})();
+startServer();
