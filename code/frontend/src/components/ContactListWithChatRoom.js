0c1601137df3a5156419de30b301425e10ee7cf6
Juliho Castillo <julihocc@users.noreply.github.com>
2024-01-19 17:50:34 -0600

    Refactor ContactListWithChatRoom component

--- code/frontend/src/components/ContactListWithChatRoom.js
+++ code/frontend/src/components/ContactListWithChatRoom.js
@@ -1,70 +1,74 @@
 // frontend\src\components\ContactListWithChatRoom.js
 import React, { useEffect } from "react";
-import {useGetContactsWithChatRoom} from "../hooks/queries/useGetContactsWithChatRoom";
-import {Typography} from "@mui/material";
-import {useTranslation} from "react-i18next";
-import {Link} from "react-router-dom";
+import { useGetContactsWithChatRoom } from "../hooks/queries/useGetContactsWithChatRoom";
+import { Alert, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 import { useSubscription } from "@apollo/client";
 import log from "../utils/logger";
 import { useState } from "react";
 import { NEW_CONTACT } from "../gql/subscriptions/NEW_CONTACT";
 
 export const ContactListWithChatRoom = () => {
-	const {t} = useTranslation();
-	const {loading, error, data} = useGetContactsWithChatRoom();
-	const {contacts, setContacts} = useState([]);
+  const { t } = useTranslation();
+  const { loading, error, data } = useGetContactsWithChatRoom();
+  const [contacts, setContacts] = useState([]);
 
-	const {
-		loading: loadingNewContact,
-		error: errorNewContact,
-		data: dataNewContact,
-	} = useSubscription(NEW_CONTACT, {
-		onComplete: () => {
-			if (data) {
-				const contactsUpdated = data.getContactsWithChatRoom;
-				setContacts(contactsUpdated);
-			}
-		},
-		onError: (err) => {
-			log.error("*Error in new contact subscription:", err);
-		},
-	});
+  const {
+    loading: loadingNewContact,
+    error: errorNewContact,
+    data: dataNewContact,
+  } = useSubscription(NEW_CONTACT, {
+    onComplete: () => {
+      if (data) {
+        const contactsUpdated = data.getContactsWithChatRoom;
+        setContacts(contactsUpdated);
+      }
+    },
+    onError: (err) => {
+      log.error("*Error in new contact subscription:", err);
+    },
+  });
 
-	useEffect(() => {
-		if (loadingNewContact) {
-			log.debug("Subscribing to new contact...");
-		}
-		if (dataNewContact) {
-			log.debug("New contact data received:", dataNewContact);
-		}
-		if (errorNewContact) {
-			log.error("Error in new contact subscription:", errorNewContact);
-		}
-		if (data) {
-			const contactsUpdated = data.getContactsWithChatRoom;
-			setContacts(contactsUpdated);
-		}
-	}, [loadingNewContact, errorNewContact, dataNewContact, data, setContacts]);
+  useEffect(() => {
+    if (loadingNewContact) {
+      log.debug("Subscribing to new contact...");
+    }
+    if (dataNewContact) {
+      log.debug("New contact data received:", dataNewContact);
+    }
+    if (errorNewContact) {
+      log.error("Error in new contact subscription:", errorNewContact);
+    }
+    if (data) {
+      const contactsUpdated = data.getContactsWithChatRoom;
+      setContacts(contactsUpdated);
+    }
+  }, [loadingNewContact, errorNewContact, dataNewContact, data]);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error : {error.message}</p>;
+  // const contacts = data.getContactsWithChatRoom;
 
-	// const contacts = data.getContactsWithChatRoom;
-
-	return (
-		<div>
-			<Typography variant="h3">{t("contacts")}</Typography>
-			<ul>
-				{contacts &&
-					contacts.map((contact) => (
-						<li key={contact._id}>
-							<Link to={`/chat/${contact.chatRoom}`} key={contact._id}>
-								Chat: {contact.email}
-							</Link>
-						</li>
-					))}
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <Typography variant="h3">{t("contacts")}</Typography>
+      {dataNewContact && (
+        <Alert severity="info">
+          New contact: {dataNewContact.newContact.email}
+        </Alert>
+      )}
+      <ul>
+        {contacts &&
+          contacts.map((contact) => (
+            <li key={contact._id}>
+              <Link to={`/chat/${contact.chatRoom}`} key={contact._id}>
+                Chat: {contact.email}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
