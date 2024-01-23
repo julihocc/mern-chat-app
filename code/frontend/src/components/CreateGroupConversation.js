5e7c8d53b4970b684b96b0d3823ddb981769eb36
Juliho Castillo <julihocc@users.noreply.github.com>
2024-01-22 18:50:21 -0600

    Refactor CreateGroupConversation component and createGroupConversationForCurrentUser mutation

--- code/frontend/src/components/CreateGroupConversation.js
+++ code/frontend/src/components/CreateGroupConversation.js
@@ -1,67 +1,113 @@
 // frontend\src\components\dashboardUtils\CreateGroupConversation.js
 
-import React, {useState} from "react";
-import {Alert, Button, CircularProgress, TextField, Typography,} from "@mui/material";
-import {useTranslation} from "react-i18next";
-import {useGetCurrentUser} from "../hooks/queries/useGetCurrentUser";
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useGetCurrentUser } from "../hooks/queries/useGetCurrentUser";
 import logger from "../utils/logger";
-import {useCreateGroupConversation} from "../hooks/mutations/useCreateGroupConversation";
-
+import { useCreateGroupConversation } from "../hooks/mutations/useCreateGroupConversation";
+// import { use } from "i18next";
+import { useEffect } from "react";
+// import { use } from "i18next";
 
 const CreateGroupConversation = () => {
-	logger.debug("CreateGroupConversation");
-	const {t} = useTranslation();
-	const [emailsInput, setEmailsInput] = useState("");
+  logger.debug("CreateGroupConversation");
+  const { t } = useTranslation();
+  const [emailsInput, setEmailsInput] = useState("");
+  const [error, setError] = useState(null);
 
+  const {
+    createGroupConversation,
+    loading: loadingCreateConversation,
+    error: errorCreateConversation,
+  } = useCreateGroupConversation();
 
-	const {createGroupConversation, loading, error} = useCreateGroupConversation();
+  useEffect(() => {
+    if (errorCreateConversation) {
+      logger.error(
+        `Error when creating conversation: ${errorCreateConversation.message}`
+      );
+      setError(errorCreateConversation.message);
+    }
+  }, [errorCreateConversation]);
 
-	const currentUser = useGetCurrentUser();
-	logger.debug("currentUser", currentUser);
-	const userEmail = currentUser.email;
-	logger.debug("userEmail", userEmail);
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+  const currentUser = useGetCurrentUser();
+  logger.debug("currentUser", currentUser);
 
-		const additionalEmails = emailsInput
-			.split(",")
-			.map((email) => email.trim());
+  const userEmail = currentUser.email;
+  logger.debug("userEmail", userEmail);
 
-		await createGroupConversation({variables: {additionalEmails}});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		if (!error) {
-			setEmailsInput("");
-		} else {
-			logger.error(error);
-			throw new Error(`CREATE_GROUP_CONVERSATION Error: ${error.message}`);
-		}
-	};
+    if (emailsInput === "") {
+      setError("Please enter at least one email address");
+      return;
+    }
 
-	return (<div>
-			<Typography variant="h3">{t("createGroupConversation")}</Typography>
-			<form onSubmit={handleSubmit}>
-				<TextField
-					id="additionalEmails"
-					label={t("additionalEmails")}
-					placeholder={t("enterCommaSeparatedEmails")}
-					variant="outlined"
-					value={emailsInput}
-					onChange={(e) => setEmailsInput(e.target.value)}
-				/>
-				<Button
-					type="submit"
-					variant="contained"
-					color="primary"
-					disabled={loading}
-				>
-					{t("create")}
-				</Button>
-			</form>
-			{loading && <CircularProgress/>}
-			{error && (<Alert severity="error">
-					CREATE_GROUP_CONVERSATION Error: {error.message}
-				</Alert>)}
-		</div>);
+    const additionalEmails = emailsInput
+      .split(",")
+      .map((email) => email.trim());
+
+    try {
+      const result = await createGroupConversation({
+        variables: { additionalEmails },
+      });
+      logger.debug("result: ", result);
+
+      logger.debug("Conversation created successfully");
+      setEmailsInput("");
+    } catch (err) {
+      logger.error(`Error creating conversation: ${err}`);
+      setError(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant="h3">{t("createGroupConversation")}</Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="additionalEmails"
+          label={t("additionalEmails")}
+          placeholder={t("enterCommaSeparatedEmails")}
+          variant="outlined"
+          value={emailsInput}
+          onChange={(e) => setEmailsInput(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disabled={loadingCreateConversation}
+        >
+          {t("create")}
+        </Button>
+      </form>
+      {loadingCreateConversation && <CircularProgress />}
+      {/* {errorCreateConversation && (
+        <Alert severity="error">
+          Error when creating conversation: {errorCreateConversation.message}
+        </Alert>
+      )} */}
+      {error && (
+        <Alert
+          severity="error"
+          onClose={() => {
+            setError(null);
+          }}
+        >
+          {error}
+        </Alert>
+      )}
+    </div>
+  );
 };
 
 export default CreateGroupConversation;
--- code/gateway/src/graphql/mutations/createGroupConversationForCurrentUser.js
+++ code/gateway/src/graphql/mutations/createGroupConversationForCurrentUser.js
@@ -1,47 +1,67 @@
-const logger = require('../../utils/logger');
-const {AuthenticationError} = require('apollo-server-express');
-const pubSub = require('../../utils/pubsub');
+const logger = require("../../utils/logger");
+const { AuthenticationError } = require("apollo-server-express");
+const pubSub = require("../../utils/pubsub");
+const e = require("express");
 
 const createGroupConversationForCurrentUser = async (_, args, context) => {
-	const {additionalEmails} = args;
-	logger.debug(`additionalEmails: ${additionalEmails}`);
-	const {token} = context;
-	const user = await context.dataSources.authAPI.getUserByToken(token);
-	if (!user) {
-		logger.error(`user not found`);
-		throw new AuthenticationError("Invalid token");
-	}
-	const otherUsers = await context.dataSources.authAPI.getManyUsersByEmail(additionalEmails);
-	const users = [user, ...otherUsers];
+  try {
 
-	if (users.length === 0) {
-		logger.error(`No users found`);
-		throw new Error("No users found");
-	} else {
-		for (let i = 0; i < users.length; i++) {
-			logger.debug(`users[${i}]: ${JSON.stringify(users[i])}`)
-		}
-	}
+	let errorMessage = null;
 
-	const participantIds = users.map((user) => user._id);
+    const { additionalEmails } = args;
+    logger.debug(`additionalEmails: ${additionalEmails}`);
+    const { token } = context;
+    const user = await context.dataSources.authAPI.getUserByToken(token);
+    if (!user) {
+      logger.error(`user not found`);
+    //   throw new AuthenticationError("Invalid token");
+	  errorMessage = "Invalid token";
+	  return
+    }
+    const otherUsers =
+      await context.dataSources.authAPI.getManyUsersByEmail(additionalEmails);
+    const users = [user, ...otherUsers];
 
-	const existingChatRoom = await context.dataSources.chatAPI.getChatRoomByParticipantIds(participantIds);
+    if (users.length === 0) {
+      logger.error(`No users found`);
+    //   throw new Error("No users found");
+	errorMessage = "No users found";
+	return
+    } else {
+      for (let i = 0; i < users.length; i++) {
+        logger.debug(`users[${i}]: ${JSON.stringify(users[i])}`);
+      }
+    }
 
-	if (existingChatRoom) {
-		logger.debug(`existingChatRoom: ${JSON.stringify(existingChatRoom)}`);
-		throw new Error("Chat room already exists");
-	}
+    const participantIds = users.map((user) => user._id);
 
-	try {
-		const chatRoom = await context.dataSources.chatAPI.createChatRoomWithParticipantIds(participantIds);
-		await context.dataSources.contactAPI.createChatRoomWithParticipantIds(chatRoom._id, participantIds);
-		logger.debug(`chatRoom: ${JSON.stringify(chatRoom)}`);
-		pubSub.publish('NEW_CHAT_ROOM', {newChatRoom: chatRoom});
-		return chatRoom;
-	} catch (err) {
-		logger.error(`Error creating chat room: ${err}`);
-		throw new Error("Error creating chat room");
-	}
-}
+    const existingChatRoom =
+      await context.dataSources.chatAPI.getChatRoomByParticipantIds(
+        participantIds
+      );
 
-module.exports = {createGroupConversationForCurrentUser};
+    if (existingChatRoom) {
+      logger.debug(`existingChatRoom: ${JSON.stringify(existingChatRoom)}`);
+    //   throw new Error("Chat room already exists!!!");
+		errorMessage = "Chat room already exists!!!";
+		throw new Error("Chat room already exists!!!");
+    }
+
+    const chatRoom =
+      await context.dataSources.chatAPI.createChatRoomWithParticipantIds(
+        participantIds
+      );
+    await context.dataSources.contactAPI.createChatRoomWithParticipantIds(
+      chatRoom._id,
+      participantIds
+    );
+    logger.debug(`chatRoom: ${JSON.stringify(chatRoom)}`);
+    pubSub.publish("NEW_CHAT_ROOM", { newChatRoom: chatRoom });
+    return chatRoom;
+  } catch (err) {
+    logger.error(`Error creating chat room: ${err}`);
+    throw new Error(`Error creating chat room: ${err}`);
+  }
+};
+
+module.exports = { createGroupConversationForCurrentUser };
