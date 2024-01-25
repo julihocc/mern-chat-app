21b08eae79ca7a00915986970fc0d6ecebbd2b8a
Juliho Castillo <julihocc@users.noreply.github.com>
2024-01-24 19:17:35 -0600

    Dashboard is only shown if someone is logged

--- code/frontend/src/MainRoutes.js
+++ code/frontend/src/MainRoutes.js
@@ -48,74 +48,89 @@ const MainRoutes = () => {
 		setDrawerOpen(!drawerOpen);
 	};
 
-	return (<>
-		<Container>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton edge="start" color="inherit" onClick={toggleDrawer}>
-						<MenuIcon/>
-					</IconButton>
-					<Typography variant="h6" style={{flexGrow: 1}}>
-						{t("welcome")}
-					</Typography>
-					<LanguageSwitcher/>
-					{isLoggedIn ? (<>
-						<Button color="inherit" onClick={handleLogout}>
-							{t("logout")}
-						</Button>
-						<Button color="inherit" component={Link} to="/dashboard">
-							{t("dashboard")}
-						</Button>
-					</>) : (<>
-						<Button color="inherit" component={Link} to="/login">
-							{t("login")}
-						</Button>
-						<Button color="inherit" component={Link} to="/signup">
-							{t("signup")}
-						</Button>
-					</>)}
-				</Toolbar>
-			</AppBar>
-			<Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
-				<List>
-					<ListItem button component={Link} to="/dashboard">
+	return (
+    <>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={toggleDrawer}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+              {t("welcome")}
+            </Typography>
+            <LanguageSwitcher />
+            {isLoggedIn ? (
+              <>
+                <Button color="inherit" onClick={handleLogout}>
+                  {t("logout")}
+                </Button>
+                <Button color="inherit" component={Link} to="/dashboard">
+                  {t("dashboard")}
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button color="inherit" component={Link} to="/login">
+                  {t("login")}
+                </Button>
+                <Button color="inherit" component={Link} to="/signup">
+                  {t("signup")}
+                </Button>
+              </>
+            )}
+          </Toolbar>
+        </AppBar>
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+          <List>
+            {/* <ListItem button component={Link} to="/dashboard">
 						<ListItemText primary="Dashboard"/>
-					</ListItem>
-				</List>
-			</Drawer>
-		</Container>
+					</ListItem> */}
+            {isLoggedIn && (
+              <ListItem button component={Link} to="/dashboard">
+                <ListItemText primary="Dashboard" />
+              </ListItem>
+            )}
+            {isLoggedIn && (
+              <ListItem button component={Link} to="/settings">
+                <ListItemText primary={t("settings")} />
+              </ListItem>
+            )}
+          </List>
+        </Drawer>
+      </Container>
 
-		<Container style={{paddingTop: "64px"}}>
-			<Routes>
-				<Route
-					path="/"
-					element={isLoggedIn ? (<Dashboard onLogout={handleLogout}/>) : (<Navigate to="/" replace/>)}
-				/>
-				<Route
-					path="/signup"
-					element={<Signup onLogin={handleLogin}/>}
-				/>
-				<Route
-					path="/login"
-					element={<Login onLogin={handleLogin}/>}
-				/>
-				<Route
-					path="/dashboard"
-					element={isLoggedIn ? (<Dashboard onLogout={handleLogout}/>) : (<Navigate to="/" replace/>)}
-				/>
-				<Route
-					path="/chat/:chatRoomId"
-					element={<ChatRoomViewer/>}
-				/>
-				<Route
-					path="/settings"
-					element={<Settings/>}
-				/>
-				<Route path="/logout" element={<Logout/>}/>
-
-			</Routes>
-		</Container>
-	</>);
+      <Container style={{ paddingTop: "64px" }}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? (
+                <Dashboard onLogout={handleLogout} />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            }
+          />
+          <Route path="/signup" element={<Signup onLogin={handleLogin} />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route
+            path="/dashboard"
+            element={
+              isLoggedIn ? (
+                <Dashboard onLogout={handleLogout} />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            }
+          />
+          <Route path="/chat/:chatRoomId" element={<ChatRoomViewer />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </Container>
+    </>
+  );
 };
 
 export default MainRoutes;
